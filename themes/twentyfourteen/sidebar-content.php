07c14e5543160e6a24bfc38804435bc5aba48148
Vitaly A <crazyyy@gmail.com>
2014-12-13 20:31:40 +0200

    adsense ready

--- themes/twentyfourteen/sidebar-content.php
+++ themes/twentyfourteen/sidebar-content.php
@@ -12,11 +12,13 @@ if ( ! is_active_sidebar( 'sidebar-2' ) ) {
 }
 ?>
 <div id="content-sidebar" class="content-sidebar widget-area" role="complementary">
-	<ins class="adsbygoogle adslot_3"
-    	style="display:block"
-     	data-ad-client="ca-pub-7907557357919250"
-     	data-ad-slot="1792862717"
-     	data-ad-format="auto"></ins>
-
+	<div class="adaptive3-container">
+		<ins class="adsbygoogle adaptive3"
+	    	style="display:block"
+	     	data-ad-client="ca-pub-7907557357919250"
+	     	data-ad-slot="1792862717"
+	     	data-ad-format="auto"></ins>
+	</div>
+	<!-- /.adaptive3-container -->
 	<?php dynamic_sidebar( 'sidebar-2' ); ?>
 </div><!-- #content-sidebar -->
