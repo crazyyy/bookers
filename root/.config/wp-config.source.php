3843e6c07bb1f25a83963f650f63229e98c7eb2c
Vitaly A <crazyyy@gmail.com>
2014-12-15 15:39:16 +0200

    fix

--- root/.config/wp-config.source.php
+++ root/.config/wp-config.source.php
@@ -3,4 +3,3 @@ define('FS_METHOD', 'direct');
 
 define('CACHE_READ_WHITELIST','_transient|posts WHERE ID IN|limit_login_'); // do not read from cache is sql contains these
 define('CACHE_WRITE_WHITELIST','_transient|limit_login_'); // do not reset cache if sql contains these
-
